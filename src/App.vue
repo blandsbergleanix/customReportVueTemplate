a2edbafb1982368149526621bba8e57405b4c2c0
Bernhard Landsberg <bernhard.landsberg@leanix.net>
2018-01-19 13:37:00 +0100

    Implemented table

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="report-container">
-    <div v-for="(factsheet, idx) in factsheets" :key="idx">
-      {{factsheet}}
+    <!--<div style="width:100%">
+      Comment
+    </div>-->
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Applications</td>
+            <td>IT Components</td>
+            <td>Data Objects</td>
+            <td>Business Capabilities</td>
+            <td>Projects</td>
+            <td>Provider</td>
+            <td>Processes</td>
+            <td>Interfaces</td>
+            <td>Other</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(factsheet, idx) in factsheets" :key="idx">
+            <td>{{factsheet.name}}</td>
+            <td>{{factsheet.numberOfApplications}}</td>
+            <td>{{factsheet.numberOfITComponents}}</td>
+            <td>{{factsheet.numberOfDataObjects}}</td>
+            <td>{{factsheet.numberOfBusinessCapabilities}}</td>
+            <td>{{factsheet.numberOfProjects}}</td>
+            <td>{{factsheet.numberOfProviders}}</td>
+            <td>{{factsheet.numberOfProcesses}}</td>
+            <td>{{factsheet.numberOfInterfaces}}</td>
+            <td>{{factsheet.numberOfOtherFactSheets}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -24,6 +56,18 @@ export default {
           facetFilters: [
             { facetKey: 'FactSheetTypes', keys: ['Application'] },
             { facetKey: 'hierarchyLevel', keys: ['1'] },
+            {
+              "facetKey":"lifecycle",
+              "operator":"OR",
+              "keys":["plan","phaseIn","active","phaseOut"],
+              "dateFilter":{
+                "from":"2018-01-17",
+                "to":"2018-01-17",
+                "type":"TODAY",
+                "minDate":"2013-06-01",
+                "maxDate":"2022-12-31"
+              }
+            }
           ]
         }
       }
@@ -31,6 +75,14 @@ export default {
         .then(res => {
           if (res.allFactSheets) {
             this.factsheets = res.allFactSheets.edges.map(edge => edge.node)
+            this.sumApps = 0
+            this.sumApps30 = 0
+            this.sumApps90 = 0
+            this.factsheets.forEach(factSheet => {
+              this.sumApps = this.sumApps + factSheet.numberOfApplications
+              this.sumApps30 = this.sumApps30 + factSheet.numberOfApplicationsMinus30
+              this.sumApps90 = this.sumApps90 + factSheet.numberOfApplicationsMinus90
+            });
           }
         })
         .catch(error => {
@@ -50,5 +102,15 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  .report-container
+    background white 
+    color black
+    padding 16px
+  
+  .item
+    background white
+    color black
+    margin 8px
+    padding 8px
 
 </style>
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -3,12 +3,40 @@ export default {
     allFactSheets(filter: $filter) {
       edges {
         node {
-          id
           name
-          level
+          ... on Application {
+            numberOfApplications
+            numberOfApplicationsMinus30
+            numberOfApplicationsMinus90
+            numberOfDataObjects
+            numberOfDataObjectsMinus30
+            numberOfDataObjectsMinus90
+            numberOfITComponents
+            numberOfITComponentsMinus30
+            numberOfITComponentsMinus90
+            numberOfBusinessCapabilities
+            numberOfBusinessCapabilitiesMinus30
+            numberOfBusinessCapabilitiesMinus90
+            numberOfProjects
+            numberOfProjectsMinus30
+            numberOfProjectsMinus90
+            numberOfProviders
+            numberOfProvidersMinus30
+            numberOfProvidersMinus90
+            numberOfProcesses
+            numberOfProcessesMinus30
+            numberOfProcessesMinus90
+            numberOfInterfaces
+            numberOfInterfacesMinus30
+            numberOfInterfacesMinus90
+            numberOfOtherFactSheets
+            numberOfDataObjectsMinus30
+            numberOfDataObjectsMinus90
+            numberOfUniqueUsersLast30Days
+        }
         }
       }
     }
   }`,
-
+  
 }
